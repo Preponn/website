1eb2da700bb89b4ca5818ecbda557e839fa5bc5b
Nitya Pandey <nityapandey3031@gmail.com>
2024-09-29 09:10:47 +0530

    made it hosting worthy

--- components/LoaderComp.vue
+++ components/LoaderComp.vue
@@ -1,37 +1,57 @@
 <template>
-    <div class="vl-parent">
-        <loading v-model:active="isLoading" :can-cancel="true" :on-cancel="onCancel" :is-full-page="fullPage" />
-
-        <label><input type="checkbox" v-model="fullPage">Full page?</label>
-        <button @click.prevent="doAjax">fetch Data</button>
+    <div v-if="visible" class="loader-overlay">
+        <div class="loader"></div>
+        <p class="loader-text">Loading...</p>
     </div>
 </template>
 
 <script>
-import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/css/index.css';
-
 export default {
-    data() {
-        return {
-            isLoading: false,
-            fullPage: true
-        }
-    },
-    components: {
-        Loading
-    },
-    methods: {
-        doAjax() {
-            this.isLoading = true;
-            // simulate AJAX
-            setTimeout(() => {
-                this.isLoading = false
-            }, 5000)
-        },
-        onCancel() {
-            console.log('User cancelled the loader.')
+    props: {
+        visible: {
+            type: Boolean,
+            default: false
         }
     }
-}
+};
 </script>
+
+<style scoped>
+.loader-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgb(255, 255, 255, 1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+}
+
+.loader {
+    border: 8px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 8px solid #3498db;
+    width: 50px;
+    height: 50px;
+    animation: spin 2s linear infinite;
+}
+
+.loader-text {
+    margin: 20px;
+    font-size: 1.5em;
+    color: #3498db;
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
--- pages/trainers/dashboard.vue
+++ pages/trainers/dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <main>
+        <LoaderComp :visible="loading" />
         <div class="container-fluid">
             <div class="container h-100 py-4">
                 <div class="row align-items-center h-100">
@@ -16,13 +17,14 @@
                     <h1 v-if="this.trainer" style="padding: 5px 0px; float: left;">Welcome, {{ this.trainer.name }}!
                     </h1>
                     <button type="submit" class="btn btn-danger"
-                        style="float: right; position: relative; top: 10px; right: 10px;" @click="logout"
-                        :class="{ 'disabled': loading }">{{ loading ? "Logging Out..." : "Logout" }}</button>
+                        style="float: right; position: relative; top: 10px; right: 10px;" @click="logout">
+                        Logout
+                    </button>
                 </div>
 
 
-                <div class="col-12">
-                    <h2 class="pon-blue" v-if="this.subject" style="padding: 5px 0px;">Subject: {{ this.subject.name }}
+                <div class="col-12 pb-4">
+                    <h2 class="pon-blue" v-if="this.subject">Subject: {{ this.subject.name }}
                     </h2>
                 </div>
 
@@ -34,37 +36,48 @@
                     </div>
                 </div> -->
 
+
                 <div class="col-10">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col">S. No.</th>
-                                <th scope="col">Student</th>
-                                <th scope="col">Present/Absent</th>
-                                <th scope="col">Action</th>
-                                <th scope="col">Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(student, index) in this.students" :key="index">
-                                <td>{{ index + 1 }}</td>
-                                <td>{{ student.name }}</td>
-                                <td>
-                                    <button type="button" class="btn btn-success"
-                                        @click="markAttendance(true, student)">Present</button>
-                                    &nbsp;
-                                    <button type="button" class="btn btn-danger"
-                                        @click="markAttendance(false, student)">Absent</button>
-                                </td>
-                                <td>
-                                    {{ present }}
-                                </td>
-                                <td>
-                                    {{ new Date().toLocaleDateString() }}
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <h2 style="border-bottom: 1px solid black;" class="pb-2">Attendance: <button type="submit"
+                            class="btn btn-primary" style="float: right; position: relative;" @click="addStudent">
+                            + Add Student
+                        </button></h2>
+
+                    <div class="row">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">S. No.</th>
+                                    <th scope="col">Student</th>
+                                    <th scope="col">Present/Absent</th>
+                                    <th scope="col">Action</th>
+                                    <th scope="col">Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(student, index) in this.students" :key="index">
+                                    <td>{{ index + 1 }}</td>
+                                    <td>{{ student.name }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-success"
+                                            @click="markAttendance(true, student)"
+                                            v-if="student.present == undefined">Present</button>
+                                        &nbsp;
+                                        <button type="button" class="btn btn-danger"
+                                            @click="markAttendance(false, student)"
+                                            v-if="student.present == undefined">Absent</button>
+                                    </td>
+                                    <td>
+                                        {{ student.present === true ? 'Present' : student.present === false ? 'Absent'
+                                            : "Not Marked" }}
+                                    </td>
+                                    <td>
+                                        {{ new Date().toLocaleDateString() }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
 
@@ -95,13 +108,19 @@
 <script>
 import { account } from '@/services/appwrite';
 import { ID } from 'appwrite';
+import LoaderComp from '~/components/LoaderComp.vue';
 import { databases } from '~/services/appwrite';
+import swal from 'sweetalert';
 
 const databaseID = "66effc34001ccdbe842a";
 const trainerCollectionID = "66effcb4000252883dcf";
 const attendanceCollectionID = "66efff850030bf04993b";
+const studentCollectionID = "66effd4d000943b5e9c0";
 
 export default {
+    components: {
+        LoaderComp
+    },
     data() {
         return {
             trainer: null,
@@ -109,19 +128,19 @@ export default {
             students: [],
             account: null,
             present: null,
-            loading: false,
+            loading: true,
         };
     },
 
     methods: {
         logout() {
-            this.loading = true;
             account.deleteSession('current').then(() => {
                 this.$router.push('/');
             }).catch((error) => {
                 console.error('Logout failed', error);
             });
         },
+
 
         getTrainer() {
             return databases.getDocument(databaseID, trainerCollectionID, this.account.$id).then((d) => {
@@ -132,6 +151,34 @@ export default {
             })
         },
 
+        addStudent() {
+            swal({
+                title: "Add Student",
+                text: "Enter student name",
+                content: "input",
+                button: {
+                    text: "Add",
+                    closeModal: false,
+                },
+            }).then((name) => {
+                if (!name) throw null;
+
+                databases.createDocument(databaseID, studentCollectionID, ID.unique(), {
+                    name: name,
+                    subject: [this.subject.$id],
+
+                }).then((d) => {
+                    console.log(d);
+                    this.students.push(d);
+                    swal("Student added successfully", {
+                        icon: "success",
+                    });
+                }).catch((error) => {
+                    console.error('Add student failed', error);
+                });
+            });
+        },
+
         markAttendance(present, students) {
             databases.createDocument(databaseID, attendanceCollectionID, ID.unique(), {
                 present: present,
@@ -140,7 +187,8 @@ export default {
                 date: new Date(),
             }).then((d) => {
                 console.log(d);
-                this.present = d.present;
+                students.present = present;
+                // this.present = d.present;
                 // alert('Attendance marked');
             })
         }
@@ -162,6 +210,8 @@ export default {
             this.$router.push('/trainers/dashboard');
         }).catch(() => {
             this.$router.push('/trainers/login');
+        }).finally(() => {
+            this.loading = false;
         });
     }
 }
